64b9118a475a8cd775acee3e04ed098ac83db0e4
Puran Singh <purandixit@gmail.com>
2023-12-24 12:34:04 +0530

    servemux swapped with chi router

--- main.go
+++ main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
+
+type apiConfig struct {
+	fileServerHits int
+}
 
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	corsMux := middlewareCors(mux)
+
+	cfg := apiConfig{
+		fileServerHits: 0,
+	}
+
+	r := chi.NewRouter()
+	fsHandler := cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	r.Handle("/app/*", fsHandler)
+	r.Handle("/app", fsHandler)
+	r.Get("/healthz", handlerReadiness)
+	r.Get("/metrics", cfg.handlerMetrics)
+	r.Get("/reset", cfg.handlerReset)
+
+	corsMux := middlewareCors(r)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -20,10 +36,4 @@ func main() {
 
 	log.Printf("Serving the file from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
