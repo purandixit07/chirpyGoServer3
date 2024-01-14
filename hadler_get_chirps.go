6d872ac4558379876886c5f968669cd5ee911ef3
Puran Singh <purandixit@gmail.com>
2024-01-14 23:12:28 +0530

    sort chirps as ascending or descending

--- hadler_get_chirps.go
+++ hadler_get_chirps.go
@@ -15,15 +15,40 @@ func (cfg *apiConfig) handlerRetrieveChirps(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
+	}
+
+	authorID := -1
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = strconv.Atoi(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author id")
+			return
+		}
+	}
+
 	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		if authorID != -1 && dbChirp.AuthorID != authorID {
+			continue
+		}
 		chirps = append(chirps, Chirp{
 			ID:       dbChirp.ID,
 			Body:     dbChirp.Body,
 			AuthorId: dbChirp.AuthorID,
 		})
 	}
-	sort.Slice(chirps, func(i, j int) bool { return chirps[i].ID < chirps[j].ID })
+
+	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].ID > chirps[j].ID
+		}
+		return chirps[i].ID < chirps[j].ID
+	})
 	respondWithJson(w, http.StatusOK, chirps)
 }
 
